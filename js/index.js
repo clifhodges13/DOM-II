b9e5520897794977dc3c9d2b2950813d1ef55b50
Clif <clifhodges13@gmail.com>
2019-08-08 19:07:24 -0500

    created click event

--- js/index.js
+++ js/index.js
@@ -1,1 +1,7 @@
-// Your code goes here
+// click event
+
+const logo = document.querySelector('.logo-heading')
+
+logo.addEventListener('click', () => {
+  logo.style.animation = `shrinkAndGrow .3s ease forwards`
+})
